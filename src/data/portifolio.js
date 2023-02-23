660c6a6e08f48ffa2e0eb55b7dc1999fdd451c95
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-02-23 16:36:45 -0300

    Created portifolio object

---
+++ src/data/portifolio.js
@@ -0,0 +1,126 @@
+const CURSOR = '&#x25AF;'
+
+export default portifolio = {
+  personal_info: {
+    photo: './src/images/marcos-adriano.png',
+    alt: 'Foto do Marcos Adriano',
+    name_1: 'Marcos',
+    name_2: 'Adriano',
+  },
+  terminal: {
+    title_1 : `> SOU DESENVOLVEDOR WEB ${CURSOR}`,
+    title_2 : `> Olá ${CURSOR};`,
+    title_3 : `> Habilidades ${CURSOR};`,
+    title_4 : `> Projetos ${CURSOR};`,
+    title_5 : `> Contato ${CURSOR};`,
+  },
+  article: {
+    p_1: 'Como desenvolvedor web júnior, meu objetivo é aplicar os conhecimentos adquiridos em programação e design para criar soluções eficientes e atraentes na web. Tenho experiência em linguagens como HTML, CSS e JavaScript, além de frameworks como o React. Estou ansioso para aprender novas tecnologias e aprimorar minhas habilidades. Além disso, valorizo a colaboração em equipe. Estou sempre disposto a aprender com meus colegas e a contribuir com minhas próprias ideias.',
+    p_2: 'Durante meus estudos em programação, tive a oportunidade de desenvolver projetos práticos. Essas experiências me ensinaram a importância de um bom planejamento, colaboração com a equipe e atenção aos detalhes para garantir que as soluções finais sejam bem-sucedidas.',
+    p_3: 'Sou um profissional proativo, que busca soluções criativas para resolver problemas e que trabalha com dedicação e comprometimento.',
+  },
+  skills: [
+    {
+      name: 'HTML',
+      src: '.src/images/icons/html.svg',
+      link: 'https://html.spec.whatwg.org/multipage/',
+    },
+    {
+      name: 'CSS',
+      src: '.src/images/icons/css.svg',
+      link: 'https://www.w3.org/Style/CSS/',
+    },
+    {
+      name: 'JavaScript',
+      src: '.src/images/icons/javascript.svg',
+      link: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+    },
+    {
+      name: 'NodeJS',
+      src: '.src/images/icons/nodejs.svg',
+      link: 'https://nodejs.org/en/',
+    },
+    {
+      name: 'Git',
+      src: '.src/images/icons/git.svg',
+      link: 'https://git-scm.com/',
+    },
+    {
+      name: 'GitHub',
+      src: '.src/images/icons/github.svg',
+      link: 'https://github.com/',
+    },
+    {
+      name: 'React',
+      src: '.src/images/icons/react.svg',
+      link: 'https://pt-br.reactjs.org/',
+    },
+    {
+      name: 'Redux',
+      src: '.src/images/icons/redux.svg',
+      link: 'https://react-redux.js.org/',
+    },
+    {
+      name: 'Bootstrap',
+      src: '.src/images/icons/bootstrap.svg',
+      link: 'https://getbootstrap.com/',
+    },
+    {
+      name: 'Jest',
+      src: '.src/images/icons/jest.svg',
+      link: 'https://jestjs.io/pt-BR/',
+    },
+    {
+      name: 'NPM',
+      src: '.src/images/icons/npm.svg',
+      link: 'https://www.npmjs.com/',
+    },
+    {
+      name: 'VSCode',
+      src: '.src/images/icons/vscode.svg',
+      link: 'https://code.visualstudio.com/',
+    },
+    {
+      name: 'Slack',
+      src: '.src/images/icons/slack.svg',
+      link: 'https://slack.com/intl/pt-br',
+    },
+    {
+      name: 'Linux',
+      src: '.src/images/icons/linux.svg',
+      link: 'https://ubuntu.com/',
+    },
+  ],
+  projects: [
+    {
+      name: 'Solar System',
+      src: './src/imagens/projects/solar-system.jpg',
+      link: 'https://solar-system-mati.surge.sh/', 
+      description: 'HTML, CSS, JavaScrpt e CSS - Mostra todas a missões no sistema solar. Clique nos planetas para ver suas óbitas.',
+    },
+    {
+      name: '',
+      src: './src/imagens/projects/.jpg',
+      link: '',
+      description: '',
+    },
+  ],
+  contact: [
+    {
+      src: '.src/images/icons/email.svg',
+      link: 'marcosadriano.ti@gmail.com',
+    },
+    {
+      src: '.src/images/icons/linkedin.svg',
+      link: 'https://www.linkedin.com/in/marcos-adriano-ti/',
+    },
+    {
+      src: '.src/images/icons/github.svg',
+      link: 'https://github.com/marcosadrianoti',
+    },     
+    {
+      src: '.src/images/icons/whatsapp.svg',
+      link: '(62) 99253-5292',
+    },      
+  ],
+}
