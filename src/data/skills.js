11568f389231e659173c6820b62ce585f8e605f4
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-10-07 17:39:46 -0300

    Created skills file.

---
+++ src/data/skills.js
@@ -0,0 +1,141 @@
+const ICON_PATH = './src/images/icons/';
+
+const skills = [
+  {
+    name: 'HTML',
+    src: `${ICON_PATH}html.svg`,
+    link: 'https://html.spec.whatwg.org/multipage/',
+  },
+  {
+    name: 'CSS',
+    src: `${ICON_PATH}css.svg`,
+    link: 'https://www.w3.org/Style/CSS/',
+  },
+  {
+    name: 'JavaScript',
+    src: `${ICON_PATH}javascript.svg`,
+    link: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+  },
+  {
+    name: 'TypeScript',
+    src: `${ICON_PATH}typescript.svg`,
+    link: 'https://www.typescriptlang.org/',
+  },
+  {
+    name: 'NodeJS',
+    src: `${ICON_PATH}nodejs.svg`,
+    link: 'https://nodejs.org/en/',
+  },
+  {
+    name: 'NPM',
+    src: `${ICON_PATH}npm.svg`,
+    link: 'https://www.npmjs.com/',
+  },
+  {
+    name: 'Git',
+    src: `${ICON_PATH}git.svg`,
+    link: 'https://git-scm.com/',
+  },
+  {
+    name: 'GitHub',
+    src: `${ICON_PATH}github.svg`,
+    link: 'https://github.com/',
+  },
+  {
+    name: 'React',
+    src: `${ICON_PATH}react.svg`,
+    link: 'https://pt-br.reactjs.org/',
+  },
+  {
+    name: 'Redux',
+    src: `${ICON_PATH}redux.svg`,
+    link: 'https://react-redux.js.org/',
+  },
+  {
+    name: 'Bootstrap',
+    src: `${ICON_PATH}bootstrap.svg`,
+    link: 'https://getbootstrap.com/',
+  },
+  {
+    name: 'Jest',
+    src: `${ICON_PATH}jest.svg`,
+    link: 'https://jestjs.io/pt-BR/',
+  },
+  {
+    name: 'Beautiful Soup',
+    src: `${ICON_PATH}beautiful_soup.svg`,
+    link: 'https://beautiful-soup-4.readthedocs.io/en/latest/',
+  },
+  {
+    name: 'Django',
+    src: `${ICON_PATH}django.svg`,
+    link: 'https://www.djangoproject.com/',
+  },
+  {
+    name: 'Docker',
+    src: `${ICON_PATH}docker.svg`,
+    link: 'https://www.docker.com/',
+  },
+  {
+    name: 'Express',
+    src: `${ICON_PATH}express_js.svg`,
+    link: 'https://expressjs.com/',
+  },
+  {
+    name: 'Flask',
+    src: `${ICON_PATH}flask.svg`,
+    link: 'https://flask.palletsprojects.com/',
+  },
+  {
+    name: 'JWT',
+    src: `${ICON_PATH}jwt.svg`,
+    link: 'https://jwt.io/',
+  },
+  {
+    name: 'MongoDB',
+    src: `${ICON_PATH}mongodb.svg`,
+    link: 'https://www.mongodb.com/',
+  },
+  {
+    name: 'Python',
+    src: `${ICON_PATH}python.svg`,
+    link: 'https://www.python.org/',
+  },
+  {
+    name: 'RTL',
+    src: `${ICON_PATH}rtl.png`,
+    link: 'https://testing-library.com/',
+  },
+  {
+    name: 'Selenium',
+    src: `${ICON_PATH}selenium.svg`,
+    link: 'https://www.selenium.dev/',
+  },
+  {
+    name: 'Sequelize',
+    src: `${ICON_PATH}sequelize.svg`,
+    link: 'https://sequelize.org/',
+  },
+  {
+    name: 'SQL',
+    src: `${ICON_PATH}sql.svg`,
+    link: 'https://www.w3schools.com/sql/',
+  },
+  {
+    name: 'VSCode',
+    src: `${ICON_PATH}vscode.svg`,
+    link: 'https://code.visualstudio.com/',
+  },
+  {
+    name: 'Slack',
+    src: `${ICON_PATH}slack.svg`,
+    link: 'https://slack.com/intl/pt-br',
+  },
+  {
+    name: 'Linux',
+    src: `${ICON_PATH}linux.svg`,
+    link: 'https://ubuntu.com/',
+  },
+]
+
+export { ICON_PATH, skills };
